f3dccbc53a58e1e29734cbd6d048562c449ed6c2
I-M-Saurav <sauravkumad1507@gmail.com>
2025-10-06 17:33:52 +0530

    Create missing_number.cpp

---
+++ Introductory_Problems/missing_number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+void solve()
+{
+ 
+   long long n,sum=0;
+   cin>>n;
+   vector<int> arr(n-1);
+   for(int i=0;i<n-1;i++){
+    cin>>arr[i];
+    sum+=arr[i];
+   }
+   long long ans=(n*(n+1))/2 -sum;
+   cout<<ans<<"\n";
+}
+int main()
+{
+ 
+    solve();
+}
