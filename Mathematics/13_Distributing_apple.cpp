dcffc039e2648c22793989fd89d32f837c576999
I-M-Saurav <sauravkumad1507@gmail.com>
2026-06-07 19:06:27 +0530

    Imeplemented solution to this problem

---
+++ Mathematics/13_Distributing_apple.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define eb emplace_back
+#define mpp make_pair
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+const ll INF = (ll)1e18;
+const int mod = 1000000007;
+
+ll modularBinaryExponentiation(int base, int exponent)
+{
+    if (exponent == 0)
+        return 1;
+    ll result = modularBinaryExponentiation(base, exponent / 2);
+    if (exponent % 2 == 1)
+    {
+        return (((result * result) % mod) * base) % mod;
+    }
+    return (result * result) % mod;
+}
+vector<ll> dp(2e6 + 2);
+void test()
+{
+    dp[0] = dp[1] = 1;
+    for (int i = 2; i <= 2e6 + 1; i++)
+    {
+        dp[i] = (dp[i - 1] * i) % mod;
+    }
+}
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    ll x = dp[n + m - 1] % mod;
+    ll y = (dp[n - 1]%mod * dp[m]%mod)%mod;
+    y = modularBinaryExponentiation((int)y, mod-2)%mod;
+    ll ans = (x * y)%mod;
+    cout<<ans<<"\n";
+    return;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    test();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
